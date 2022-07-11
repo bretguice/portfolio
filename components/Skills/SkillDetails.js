92354dcb0436359f111ef38199211f2e9feb166d
bretguice <89871624+bretguice@users.noreply.github.com>
2022-07-11 15:33:02 -0500

    updated skill img sizing

--- components/Skills/SkillDetails.js
+++ components/Skills/SkillDetails.js
@@ -4,9 +4,9 @@ function SkillDetails({skill}) {
     return (
         <>
         <div className={classes.container}>
-            <h5 className={classes.title}>{skill.title}</h5>
-            <div className={classes.imgContainer}>
-                <Image className={classes.img} src={skill.img} alt={skill.title}/>
+            {/* <h5 className={classes.title}>{skill.title}</h5> */}
+            <div layout="responsive" sizes="150px" className={classes.imgContainer}>
+                <Image  className={classes.img} src={skill.img} alt={skill.title}/>
             </div>
         </div>
         </>

5ff0d26d2ffaaf7cd0b4ffa1187cc95d44d64f39
bretguice <89871624+bretguice@users.noreply.github.com>
2022-07-11 14:22:22 -0500

    more changes to about

--- components/About/AboutContent.js
+++ components/About/AboutContent.js
@@ -4,8 +4,8 @@ function AboutContent() {
     return(
         <div className={classes.container}>
             <p className={classes.content}>
-            After spending 10 years in athletics working primarily in ticketing, I decided it was time to find a new career path.  I am a self-taught full stack web developer.  
-            My journey as a developer officially started in September 2021 but unofficially began years earlier.  Throughout my time in ticketing,  I spent my spare time exploring 
+            After spending 10 years in athletics working primarily in ticketing, I decided to take the plunge towards a new career path.  I am a self-taught full stack web developer.  
+            My journey as a developer officially started in September 2021, but unofficially began years earlier.  Throughout my ticketing career,  I spent my spare time exploring 
             the functionality of the software to see if there were improvements I could make.  The biggest tool I tackled was attempting to fit my company and our unique business 
             with a CRM.  This process introduced me to thinking like a developer and was the launching point for me to make a career change into web development.  
             </p>

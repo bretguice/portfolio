f12c4813e2c9e9bd971ca82b5e4b3961de1075a0
bretguice <89871624+bretguice@users.noreply.github.com>
2022-07-11 14:15:34 -0500

    updated about page

--- components/About/AboutContent.js
+++ components/About/AboutContent.js
@@ -4,10 +4,10 @@ function AboutContent() {
     return(
         <div className={classes.container}>
             <p className={classes.content}>
-            After spending 10 years in the athletics working primarily in ticketing, I decided it was time to find a new career path.  I am a self-taught full stack web developer.  
-            My journey as a developer officially started in September of 2021 but unofficially began years earlier.  I would always seem to find my spare time spent exploring the
-            functionality of the software I was using to see if there were improvements I could make.  The biggest tool I tackled was attmepting to fit my company and our unique business 
-            with a CRM.  It is through this process that I credit to me truly learning to think like a developer even though I had yet to learn any coding skills.  
+            After spending 10 years in athletics working primarily in ticketing, I decided it was time to find a new career path.  I am a self-taught full stack web developer.  
+            My journey as a developer officially started in September 2021 but unofficially began years earlier.  Throughout my time in ticketing,  I spent my spare time exploring 
+            the functionality of the software to see if there were improvements I could make.  The biggest tool I tackled was attempting to fit my company and our unique business 
+            with a CRM.  This process introduced me to thinking like a developer and was the launching point for me to make a career change into web development.  
             </p>
         </div>
     )
